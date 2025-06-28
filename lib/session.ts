bbefa6e82c2bbbd627186a723640341c807d09ec
eudropco <mertnakis@aol.com>
2025-06-28 11:39:00 +0300

    sadqwasx

--- lib/session.ts
+++ lib/session.ts
@@ -1,5 +1,4 @@
 import { getIronSession } from 'iron-session';
-// DEĞİŞİKLİK 1: 'type CookieStore' eklendi.
 import { cookies } from 'next/headers';
 import { User } from '@prisma/client';
 import prisma from '@/lib/prisma';
@@ -18,21 +17,20 @@ const sessionOptions = {
 
 export async function getCurrentUser(): Promise<User | null> {
   try {
-    const cookieStore = cookies();
-    
-    // DEĞİŞİKLİK 2: 'as CookieStore' eklenerek tip zorlaması yapıldı.
-    const session = await getIronSession<SessionData>(await cookies(), sessionOptions);
+    // await eksikti
+    const cookieStore = await cookies();
+
+    const session = await getIronSession<SessionData>(cookieStore, sessionOptions);
 
     if (!session.userId) {
       return null;
     }
-    
+
     const user = await prisma.user.findUnique({
       where: { id: session.userId },
     });
 
     return user;
-  
   } catch (error) {
     return null;
   }
